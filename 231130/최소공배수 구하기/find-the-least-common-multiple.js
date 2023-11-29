641f60faba15bacc2fef4c884617ca1fcb2e9159
donghyeon choi <downpool.choi@gmail.com>
2023-11-30 07:17:49 +0900

    Title: 최소공배수 구하기, Time: 227ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 231130/최소공배수 구하기/find-the-least-common-multiple.js
@@ -0,0 +1,15 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
+
+// let dx = [1, 0, -1, 0];
+// let dy = [0, -1, 0, 1];
+
+
+function gcd(a, b){
+    if (b == 0){
+        return a;
+    }else{
+        return gcd(b, a % b);
+    }
+}
+
+console.log((input[0] * input[1]) / gcd(input[0], input[1]));
