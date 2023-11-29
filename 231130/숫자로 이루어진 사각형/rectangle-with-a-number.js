ee51c1110d6a7a44771809bfdf650ac15ec7916d
donghyeon choi <downpool.choi@gmail.com>
2023-11-30 07:05:03 +0900

    Title: 숫자로 이루어진 사각형, Time: 229ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 231130/숫자로 이루어진 사각형/rectangle-with-a-number.js
@@ -0,0 +1,22 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('').map(Number);
+
+// let dx = [1, 0, -1, 0];
+// let dy = [0, -1, 0, 1];
+
+// for(let i = 0; i < input * input; i++){
+//     for(let j = 1; j < )
+// }
+
+
+function printNum(n) {
+    let cnt = 1;
+    for(let i = 0; i < n; i++){
+        for(let j = 0; j < n; j++){
+            process.stdout.write(cnt++ + " ");
+            cnt = cnt >= 10 ? cnt % 10 + 1 : cnt;
+        }
+        console.log('');
+    }
+}
+
+printNum(input);
