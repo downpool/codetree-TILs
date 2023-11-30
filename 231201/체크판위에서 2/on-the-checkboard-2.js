7ff9bee6797f8a7018c607c318b73405674eb19b
donghyeon choi <downpool.choi@gmail.com>
2023-12-01 06:32:53 +0900

    Title: 체크판위에서 2, Time: 283ms, Memory: 45MB, Status: Passed - CodeTree

--- 231201/체크판위에서 2/on-the-checkboard-2.js
+++ 231201/체크판위에서 2/on-the-checkboard-2.js
@@ -1,22 +1,21 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 let RandC = input[0].split(' ').map(Number);
-
+let n = RandC[0];
+let m = RandC[1];
 let array = Array.from(Array(RandC[0]), () => Array(RandC[1]));
 for(let i = 1; i < input.length; i++){
     array[i - 1] = input[i].split(' ');
 }
-let start = array[0][0];
 
-let max = Number.MIN_SAFE_INTEGER;
-for(let i = 1; i < RandC[0] - 1; i++){
-    for(let j = 1; j < RandC[1] - 1; j++){
-        if(array[i][j] != start){
-            max = Math.max(max, (Math.max(1, (i - 2) * (j - 2))) * ((RandC[0] - i - 2) * (RandC[1] - j - 2)));
-        }
-    }
-}
-if (start == array[RandC[0] - 1][RandC[1] - 1]){
-    console.log(0);
-}else{
-    console.log(max);
-}
+let cnt = 0;
+for(let i = 1; i < n; i++)
+    for(let j = 1; j < m; j++)
+        for(let k = i + 1; k < n - 1; k++)
+            for(let l = j + 1; l < m - 1; l++)
+                // 그 중 색깔이 전부 달라지는 경우에만 개수를 세줍니다.
+                if(array[0][0] != array[i][j] && 
+                   array[i][j] != array[k][l] &&
+                   array[k][l] != array[n - 1][m - 1])
+                    cnt++;
+                        
+console.log(cnt);
