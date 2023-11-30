e0345dad4416e215454a4df79b788fce4481b6c3
donghyeon choi <downpool.choi@gmail.com>
2023-12-01 06:27:43 +0900

    Title: 체크판위에서 2, Time: 282ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 231201/체크판위에서 2/on-the-checkboard-2.js
@@ -0,0 +1,17 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let RandC = input[0].split(' ').map(Number);
+
+let array = Array.from(Array(RandC[0]), () => Array(RandC[1]));
+for(let i = 1; i < input.length; i++){
+    array[i - 1] = input[i].split(' ');
+}
+let start = array[0][0];
+let max = Number.MIN_SAFE_INTEGER;
+for(let i = 1; i < RandC[0] - 1; i++){
+    for(let j = 1; j < RandC[1] - 1; j++){
+        if(array[i][j] != start){
+            max = Math.max(max, (Math.max(1, (i - 2) * (j - 2))) * ((RandC[0] - i - 2) * (RandC[1] - j - 2)));
+        }
+    }
+}
+console.log(max);
