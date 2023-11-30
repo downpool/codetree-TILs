678b065ad3ff8b28fa7c7e1bc2173b4cff70acf4
donghyeon choi <downpool.choi@gmail.com>
2023-12-01 05:52:07 +0900

    Title: 일렬로 서있는 소 2, Time: 232ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 231201/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.js
+++ 231201/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.js
@@ -2,8 +2,8 @@ let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n
 let n = Number(input[0]);
 let line = input[1].split(' ').map(Number);
 let count = 0;
-for(let i = 0; i < n; i++){
-    for(let j = i + 1; j < n; j++){
+for(let i = 0; i < n - 2; i++){
+    for(let j = i + 1; j < n - 1; j++){
         for(let k = j + 1; k < n; k++){
             if (line[i] < line[j] && line[j] < line[k]){
                 count++;
