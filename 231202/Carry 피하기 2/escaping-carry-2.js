848d5fb125c21a0fa4910d144caa25504b0f2c2f
donghyeon choi <downpool.choi@gmail.com>
2023-12-02 08:53:40 +0900

    Title: Carry 피하기 2, Time: 269ms, Memory: 13MB, Status: Wrong Answer - CodeTree

--- 231202/Carry 피하기 2/escaping-carry-2.js
+++ 231202/Carry 피하기 2/escaping-carry-2.js
@@ -1,6 +1,6 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 let max = Number.MIN_SAFE_INTEGER;
-for(let i = 0; i < input.length - 2; i++){
+for(let i = 1; i < input.length - 2; i++){
     for(let j = i + 1; j < input.length - 1; j++){
         for(let k = j + 1; k < input.length; k++){
             let ok = true;
