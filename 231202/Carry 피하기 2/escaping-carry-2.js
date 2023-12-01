24601a48bc08ee576d1cf7c75b29d9d07c0cccc3
donghyeon choi <downpool.choi@gmail.com>
2023-12-02 08:48:38 +0900

    Title: Carry 피하기 2, Time: 206ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 231202/Carry 피하기 2/escaping-carry-2.js
@@ -0,0 +1,22 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let max = Number.MIN_SAFE_INTEGER;
+for(let i = 0; i < input.length - 2; i++){
+    for(let j = i + 1; j < input.length - 1; j++){
+        for(let k = j + 1; k < input.length; k++){
+            let ok = true;
+            let line1 = input[i].padStart(5, "0");
+            let line2 = input[j].padStart(5, "0");
+            let line3 = input[k].padStart(5, "0");
+            for(let l = 0; l < 5; l++){
+                if(Number(line1[l]) + Number(line2[l]) + Number(line3[l]) >= 10){
+                    ok = false;
+                    break;
+                }
+            }
+            if(ok){
+                max = max < Number(line1) + Number(line2) + Number(line3) ? Number(line1) + Number(line2) + Number(line3) : max;
+            }
+        }
+    }
+}
+console.log(max);
