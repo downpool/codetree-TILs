b9d4a2d6856a22bf02e636e4ccfe15cf71949a94
donghyeon choi <downpool.choi@gmail.com>
2023-12-04 10:23:13 +0900

    Title: 십의 자리 숫자, Time: 11ms, Memory: 1MB, Status: Passed - CodeTree

--- 231204/십의 자리 숫자/number-of-tens-digit.swift
+++ 231204/십의 자리 숫자/number-of-tens-digit.swift
@@ -4,6 +4,7 @@ var line = readLine()!.split(separator: " ").map { Int($0)! }
 var nums: [Int] = Array(repeating: 0, count: 10)
 
 for i in line {
+    if i == 0 { break }
     let tens = i / 10
     if tens != 0 {
         nums[tens] += 1
