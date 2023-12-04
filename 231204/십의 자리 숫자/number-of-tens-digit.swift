d7b1592b248d20570287f502a104f7ef54dbb18e
donghyeon choi <downpool.choi@gmail.com>
2023-12-04 10:20:43 +0900

    Title: 십의 자리 숫자, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 231204/십의 자리 숫자/number-of-tens-digit.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+var line = readLine()!.split(separator: " ").map { Int($0)! }
+var nums: [Int] = Array(repeating: 0, count: 10)
+
+for i in line {
+    let tens = i / 10
+    if tens != 0 {
+        nums[tens] += 1
+    }
+    // print(tens)
+}
+
+for i in stride(from: 1, to: 10, by: 1) {
+    print("\(i) - \(nums[i])")
+}
