7355649a2a62d6def4d1a66dfbc657a90feb929b
donghyeon choi <downpool.choi@gmail.com>
2024-04-30 08:11:00 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 10ms, Memory: 1MB, Status: Passed - CodeTree

--- 240430/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.swift
+++ 240430/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.swift
@@ -2,8 +2,16 @@ import Foundation
 
 let n = Int(readLine()!)!
 
-if n % 2 == 0 && n % 5 == 0 {
+if n % 2 == 0 && sums(n) {
     print("Yes")
 }else{
     print("No")
+}
+
+func sums(_ n: Int) -> Bool {
+    var sum = 0
+    for i in String(n) {
+        sum += Int(String(i))!
+    }
+    return (sum % 5 == 0)
 }
