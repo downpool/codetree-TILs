c9110d8bc49d705690a2ffd7d6425600af4bc1e7
donghyeon choi <downpool.choi@gmail.com>
2024-05-08 15:57:01 +0900

    Title: 최대로 겹치는 구간, Time: 21ms, Memory: 1MB, Status: Runtime Error - CodeTree

--- 240508/최대로 겹치는 구간/maximum-overlapped-segments.swift
+++ 240508/최대로 겹치는 구간/maximum-overlapped-segments.swift
@@ -6,7 +6,7 @@ var array = [Int](repeating: 0, count: 201)
 
 for _ in 0..<n {
     let line = readLine()!.split(separator: " ").map({Int($0)!})
-    if line[0] + 1 >= line[1] {
+    if line[0] >= line[1] {
         continue
     }
     for i in (line[0] + 101) ... (line[1] + 99){
