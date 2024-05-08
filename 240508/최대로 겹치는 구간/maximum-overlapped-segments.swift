bfa7709e286a19e71f6b32fe81463f2dbf78aeb8
donghyeon choi <downpool.choi@gmail.com>
2024-05-08 15:56:37 +0900

    Title: 최대로 겹치는 구간, Time: 13ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 240508/최대로 겹치는 구간/maximum-overlapped-segments.swift
+++ 240508/최대로 겹치는 구간/maximum-overlapped-segments.swift
@@ -6,6 +6,9 @@ var array = [Int](repeating: 0, count: 201)
 
 for _ in 0..<n {
     let line = readLine()!.split(separator: " ").map({Int($0)!})
+    if line[0] + 1 >= line[1] {
+        continue
+    }
     for i in (line[0] + 101) ... (line[1] + 99){
         array[i] += 1
     }
