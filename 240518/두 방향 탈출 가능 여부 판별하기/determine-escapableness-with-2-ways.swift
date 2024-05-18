9e3b4affe30979e52584e285ca1162248ea32fd8
donghyeon choi <downpool.choi@gmail.com>
2024-05-18 15:03:18 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 17ms, Memory: 1MB, Status: Passed - CodeTree

--- 240518/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.swift
+++ 240518/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.swift
@@ -3,6 +3,7 @@ import Foundation
 let nm = readLine()!.split(separator: " ").map({Int($0)!})
 
 var graph = [[Int]](repeating: [Int](repeating: 0, count: nm[1]), count: nm[0])
+var visited = [[Bool]](repeating: [Bool](repeating: false, count: nm[1]), count: nm[0])
 
 for i in 0 ..< nm[0] {
     let line = readLine()!.split(separator: " ").map({Int($0)!})
@@ -13,26 +14,37 @@ for i in 0 ..< nm[0] {
 
 var ending = 0
 
-func dfs(_ x: Int, _ y: Int) {
-    if graph[x][y] == 0 {
-        return
-    }
-
-    if x == nm[0] - 1 && y == nm[1] - 1 {
-        ending = 1
-        return
-    }
-
-    if x == nm[0] - 1 {
-        dfs(x, y + 1)
-    }else if y == nm[1] - 1 {
-        dfs(x + 1, y)
+func inRange(_ x: Int, _ y: Int) -> Bool {
+    if x >= 0 && x < nm[0] && y >= 0 && y < nm[1] {
+        return true
     }else{
+        return false
+    }
+}
+
+func canGo(_ x: Int, _ y: Int) -> Bool {
+    if !inRange(x, y) {
+        return false
+    }
+    if visited[x][y] || graph[x][y] == 0 {
+        return false
+    }
+
+    return true
+}
+
+func dfs(_ x: Int, _ y: Int) {
+    if canGo(x, y + 1) {
+        visited[x][y + 1] = true
         dfs(x, y + 1)
+    }
+    if canGo(x + 1, y) {
+        visited[x + 1][y] = true
         dfs(x + 1, y)
     }
 }
 
+visited[0][0] = true
 dfs(0, 0)
 
-print(ending)
+print(visited[nm[0] - 1][nm[1] - 1] ? 1 : 0)
