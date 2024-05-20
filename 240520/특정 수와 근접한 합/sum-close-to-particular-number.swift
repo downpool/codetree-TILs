5cc07fcdd6d15d97f160bed015b765a218854596
donghyeon choi <downpool.choi@gmail.com>
2024-05-20 09:57:40 +0900

    Title: 특정 수와 근접한 합, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240520/특정 수와 근접한 합/sum-close-to-particular-number.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+let ns = readLine()!.split(separator: " ").map({Int($0)!})
+var line = readLine()!.split(separator: " ").map({Int($0)!})
+
+line.sort()
+var gap = 100000
+
+for i in 0 ..< line.count - 1 {
+    for j in i ..< line.count {
+        var temp = line.reduce(0, +) - line[i] - line[j]
+        gap = abs(ns[1] - temp) < gap ? abs(ns[1] - temp) : gap
+    }
+}
+print(gap)
