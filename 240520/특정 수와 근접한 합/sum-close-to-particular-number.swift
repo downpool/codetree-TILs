32114791340e9edc7a1a2d6179e06b5728d11618
donghyeon choi <downpool.choi@gmail.com>
2024-05-20 09:59:55 +0900

    Title: 특정 수와 근접한 합, Time: 13ms, Memory: 1MB, Status: Passed - CodeTree

--- 240520/특정 수와 근접한 합/sum-close-to-particular-number.swift
+++ 240520/특정 수와 근접한 합/sum-close-to-particular-number.swift
@@ -7,7 +7,7 @@ line.sort()
 var gap = 100000
 
 for i in 0 ..< line.count - 1 {
-    for j in i ..< line.count {
+    for j in i + 1 ..< line.count {
         var temp = line.reduce(0, +) - line[i] - line[j]
         gap = abs(ns[1] - temp) < gap ? abs(ns[1] - temp) : gap
     }
