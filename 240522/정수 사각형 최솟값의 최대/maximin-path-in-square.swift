abe4d6b36ed24478ba73a22e63e8c17bcf71a271
donghyeon choi <downpool.choi@gmail.com>
2024-05-22 14:08:53 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 18ms, Memory: 1MB, Status: Passed - CodeTree

--- 240522/정수 사각형 최솟값의 최대/maximin-path-in-square.swift
+++ 240522/정수 사각형 최솟값의 최대/maximin-path-in-square.swift
@@ -13,8 +13,8 @@ for i in 0 ..< n {
 }
 dp[0][0] = grid[0][0]
 for i in 1 ..< n {
-    dp[i][0] = min(dp[i - 1][0], dp[i][0])
-    dp[0][i] = min(dp[0][i - 1], dp[0][i])
+    dp[i][0] = min(dp[i - 1][0], grid[i][0])
+    dp[0][i] = min(dp[0][i - 1], grid[0][i])
 }
 for i in 1 ..< n {
     for j in 1 ..< n {
