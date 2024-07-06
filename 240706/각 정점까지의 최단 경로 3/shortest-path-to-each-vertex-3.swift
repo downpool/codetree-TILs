96dea8bad1c65903aec25cac72a76e445beeb508
donghyeon choi <downpool.choi@gmail.com>
2024-07-06 15:33:07 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 12ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240706/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+let nm = readLine()!.split(separator: " ").map({Int($0)!})
+let INF = 1000000000
+
+var graph = [[Int]](repeating: [Int](repeating: 0, count: nm[0] + 1), count: nm[0] + 1)
+
+var dist = [Int](repeating: INF, count: nm[0] + 1)
+var visited = [Bool](repeating: false, count: nm[0] + 1)
+
+
+dist[1] = 0
+
+for _ in 0 ..< nm[1] {
+    let line = readLine()!.split(separator: " ").map({Int($0)!})
+    graph[line[0]][line[1]] = line[2]
+}
+
+for i in 1 ... nm[0] {
+    var min_idx = -1
+    for j in 1 ... nm[0] {
+        if visited[j] {
+            continue
+        }
+        if min_idx == -1 || dist[min_idx] > dist[j] {
+            min_idx = j
+        }
+    }
+
+    visited[min_idx] = true
+
+    for j in 1 ... nm[0] {
+        if graph[min_idx][j] == 0 {
+            continue
+        }
+        dist[j] = min(dist[j], dist[min_idx] + graph[min_idx][j])
+    }
+}
+
+for i in 2 ... nm[0] {
+    if dist[nm[0]] == INF {
+        print(-1)
+    }else{
+        print(dist[i])
+    }
+}
