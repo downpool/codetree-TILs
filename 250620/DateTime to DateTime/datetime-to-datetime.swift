278e9881eee419dc723f3a301fbd3343cf283766
donghyeon choi <downpool.choi@gmail.com>
2025-06-19 18:39:39 -0700

    Title: DateTime to DateTime, Time: 17ms, Memory: 14MB, Status: Wrong Answer - Codetree

---
+++ 250620/DateTime to DateTime/datetime-to-datetime.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+let inputLine = readLine()!
+let numbers = inputLine.split(separator: " ").map { Int(String($0))! }
+let a = numbers[0]
+let b = numbers[1]
+let c = numbers[2]
+
+// Please write your code here.
+var d = a * 24 * 60 + b * 60 + c - 11 * 24 * 60 - 11 * 60 - 11
+print(d)
