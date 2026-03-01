9768979cd99dd3645043e89a0f4555e0222abccc
donghyeon choi <downpool.choi@gmail.com>
2026-03-02 07:52:40 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 29ms, Memory: 15MB, Status: Runtime Error - Codetree

---
+++ 260302/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+let firstLine = readLine()!.split(separator: " ").map { Int(String($0))! }
+let n = firstLine[0]
+let m = firstLine[1]
+var grid = [[Int]]()
+for _ in 0..<n {
+    let row = readLine()!.split(separator: " ").map { Int(String($0))! }
+    grid.append(row)
+}
+// Please write your code here.
+var ans: Int = 0
+func dfs(_ x: Int, _ y: Int) {
+    if x >= m || y >= n || x < 0 || y < 0 || grid[x][y] != 1 {
+        return
+    }
+    if x == (m - 1) && y == (n - 1) {
+        ans = 1
+        return
+    } 
+    dfs(x + 1, y)
+    dfs(x, y + 1)
+}
+dfs(0, 0)
+
+print(ans)
